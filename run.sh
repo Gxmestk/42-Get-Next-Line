6506e3dda6637fc6ef05ded4bb3fc5ac255f91d5
Gxmestk <gt.khemniwat@gmail.com>
2022-10-02 19:40:56 +0700

    fix some bug

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 clear  
 
 echo "-----------Mandatory Test----------"
-gcc -Wall -Wextra -Werror get_next_line.c get_next_line_utils.c main.c  -D BUFFER_SIZE=1 && ./a.out
+gcc -Wall -Wextra -Werror get_next_line.c get_next_line_utils.c main.c  -D BUFFER_SIZE=42 && ./a.out
 
 rm a.out
