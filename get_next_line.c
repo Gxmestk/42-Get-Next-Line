d2da8d4ccc0a6966c6ce203b119c4ee65107ea6e
Gxmestk <gt.khemniwat@gmail.com>
2022-10-03 11:46:32 +0700

    refractoring

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:48:27 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/10/03 11:38:56 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/10/03 11:46:29 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,9 @@ static void slst_split(t_slist *buffer, char *tmp)
 	{
 		n = ft_strchr(tmp, '\n');
 		if (n == 0)
-			buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+			buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1 + 10);
 		else
-			buf = (char *)malloc(sizeof(char) * (n - tmp) + 2);
+			buf = (char *)malloc(sizeof(char) * (n - tmp) + 2 + 10);
 		rbuf = buf;
 		while (tmp <= n || (*tmp != '\0' && n == 0))
 			*buf++ = *tmp++;
