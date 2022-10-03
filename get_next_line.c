5ead8ea8b451c97ba4f44ae9c19d4cd6d6cca55e
Gxmestk <gt.khemniwat@gmail.com>
2022-10-03 11:22:50 +0700

    refractoring

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:48:27 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/10/03 11:20:42 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/10/03 11:22:36 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,13 +61,13 @@ static int	read_line(t_slist *buffer)
 
 	while (1)
 	{
-		tmp = (char *)malloc(sizeof(char) * BUFFER_SIZE * 100 + 1);
+		tmp = (char *)malloc(sizeof(char) * BUFFER_SIZE * 10 + 1);
 		if (tmp == NULL)
 				return (0);
 		tmp2 = tmp;
 		while (1)
 		{
-			if (tmp2 - tmp == BUFFER_SIZE * 100	)
+			if (tmp2 - tmp == BUFFER_SIZE * 10)
 			{
 				slst_addback(buffer, tmp);
 				break ;
