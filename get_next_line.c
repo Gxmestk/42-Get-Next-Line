20d29b7d2e88d4544d3d3b966cd902d913ee789b
Gxmestk <gt.khemniwat@gmail.com>
2022-10-03 16:17:29 +0700

    Optimize 1000

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tkhemniw <gt.khemniwat@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:48:27 by tkhemniw          #+#    #+#             */
-/*   Updated: 2022/10/03 15:56:01 by tkhemniw         ###   ########.fr       */
+/*   Updated: 2022/10/03 16:16:45 by tkhemniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -176,7 +176,12 @@ char	*get_next_line(int fd)
 	if (fd < 0 || fd >= MAX_FILES || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer[fd].fd = fd;
-	buffer[fd].opt_bufs = BUFFER_SIZE * 215;
+	if (BUFFER_SIZE <= 1000)
+		buffer[fd].opt_bufs = BUFFER_SIZE * 1000;
+	else if (BUFFER_SIZE <= 100000)
+		buffer[fd].opt_bufs = BUFFER_SIZE * 10;
+	else
+		buffer[fd].opt_bufs = BUFFER_SIZE;
 	if (read_line(&buffer[fd]))
 		slst_bufcat(&rstr, &buffer[fd]);
 	//printf("rstr = %s\n",rstr);
